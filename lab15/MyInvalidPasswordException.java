dcf96f700a20c28b4538fa4c9eeb31c9315bbc2f
MarcelZama <92083030+MarcelZama@users.noreply.github.com>
2022-03-13 23:07:45 +0000

    Lab15

---
+++ lab15/MyInvalidPasswordException.java
@@ -0,0 +1,9 @@
+package ie.itcarlow.lab15;
+
+public class MyInvalidPasswordException extends Exception {
+	
+	public MyInvalidPasswordException(String error){
+		super(error);
+	}
+
+}
---
+++ lab15/MyOutOfRangeException.java
@@ -0,0 +1,16 @@
+package ie.itcarlow.lab15;
+
+
+
+public class MyOutOfRangeException extends Exception {
+	
+	public MyOutOfRangeException(String errorMessage) {
+		super(errorMessage);
+	}
+
+	
+}
+
+
+
+
